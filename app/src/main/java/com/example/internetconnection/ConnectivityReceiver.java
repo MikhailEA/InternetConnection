58d3d14a395c61437ff51f3f4068865c3d4747ec
MikhailEA <michev.work@gmail.com>
2020-10-08 18:01:31 +0300

    Check Internet Connection

---
+++ app/src/main/java/com/example/internetconnection/ConnectivityReceiver.java
@@ -0,0 +1,16 @@
+package com.example.internetconnection;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ConnectivityReceiver extends BroadcastReceiver {
+    public
+
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
